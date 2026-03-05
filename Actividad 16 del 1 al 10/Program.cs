9c68262798eec09d34323a8f663060fdbd1163e1
verno <verno@ASUS>
2026-03-05 17:09:38 -0600

    case 1

--- Actividad 16 del 1 al 10/Program.cs
+++ Actividad 16 del 1 al 10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Actividad 16 ej 1 al 10");
+Console.WriteLine("Elija una opción");
+Console.WriteLine("1. Ejercicio 1");
+int opcion = int.Parse(Console.ReadLine());
+
+
+switch (opcion)
+{
+    case 1:
+    int num1 = 1;
+    do
+    {
+    Console.WriteLine("Ejercicio 1");
+    Console.WriteLine($"{num1}");
+    num1++;
+    }
+    while (num1 <=5 );
+    break;
+}
